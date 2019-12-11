4d214cf25972cbc54f6905e4662c6fe3227ac9c8
probably-not-porter <pelibby16@earlham.edu>
2019-12-11 12:49:32 -0500

    first version of csv

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# pokedex
+# Pokedex
+
+I am not content with the pokedex offerings of the internet and the android app store. Therefore I am starting this multiphase plan to create better content so that I can REFERENCE THINGS IN PEACE.
+
+## PHASE ONE - WEB SCRAPING
+
+Phase one consists of collecting datasets for creating such a tool. I will collect my first set of data from Serebii (please dont sue me or nothin)
--- scraper.py
+++ scraper.py
@@ -2,6 +2,12 @@ from urllib.request import urlopen
 from urllib.error import HTTPError
 from urllib.error import URLError
 from bs4 import BeautifulSoup
+import csv
+
+
+data = []
+# add header
+data.append(['Dex Num', 'Name', 'Abilities', 'HP', 'ATTACK', 'DEFENSE', 'S. ATTACK', 'S. DEFENSE', 'SPEED'])
  
 try:
     html = urlopen("https://www.serebii.net/pokemon/all.shtml")
@@ -18,6 +24,31 @@ else:
         for i in range(2, len(inf)):
             stats = inf[i].findAll("td", {"class": "fooinfo"})
             if stats != []:
-                dexnum = stats[0].getText()
-                name = stats[2].getText()
-                ability = stats[4].getText()
+                dexnum = stats[0].getText().replace("\n", "").replace("\t", "")
+                name = stats[2].getText().replace("\n", "").replace("\t", "")
+                ability = stats[4].getText().replace("\n", "").replace("\t", "")
+                health = stats[5].getText().replace("\n", "")
+                attack = stats[6].getText().replace("\n", "")
+                defense = stats[7].getText().replace("\n", "")
+                sattack = stats[8].getText().replace("\n", "")
+                sdefense = stats[9].getText().replace("\n", "")
+                speed = stats[10].getText().replace("\n", "")
+
+                # create row
+                data_item = []
+                data_item.append(dexnum)
+                data_item.append(name)
+                data_item.append(ability)
+                data_item.append(health)
+                data_item.append(attack)
+                data_item.append(defense)
+                data_item.append(sattack)
+                data_item.append(sdefense)
+                data_item.append(speed)
+                data.append(data_item)
+ 
+# create datafile
+myFile = open('all.csv', 'w')
+with myFile:
+   writer = csv.writer(myFile)
+   writer.writerows(data)
