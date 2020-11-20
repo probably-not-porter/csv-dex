a4ff456ae91b00e259bb9314e92e357ca6b77e04
probably-not-porter <pelibby16@earlham.edu>
2020-11-20 15:30:40 -0500

    analysis 2

--- tools/analysis.py
+++ tools/analysis.py
@@ -1,40 +1,53 @@
 import csv
 
 # Perform Viability analysis
-
 def main():
-    results = []
-    data = []
-    with open("../data/SWSH.csv") as csvfile:
+    opponent_data = "../data/SWSH.csv"
+    candidate_data = "../data/SWSH.csv"
+
+    analysis(opponent_data, candidate_data)
+
+def analysis(opp_dir, candidates_dir):
+    opp = []
+    candidates = []
+    
+    with open(opp_dir) as csvfile:
         reader = csv.reader(csvfile) # change contents to floats
         for row in reader: # each row is a list
-            results.append(row)
+            opp.append(row)
+
+    with open(candidates_dir) as csvfile:
+        reader = csv.reader(csvfile) # change contents to floats
+        for row in reader: # each row is a list
+            candidates.append(row)
 
     print("--> Create Stat Order")
-    stat_order = Sort(results[1:], 14)
+    stat_order = Sort(opp[1:], 14)
 
-    for pokemon in stat_order:
+    data = []
+
+    for pokemon in candidates[1:]:
         name = pokemon[1]
         total = pokemon[14]
         def_score = 0
         att_score = 0
 
-        print("--> Stat Calc: " + name)
+        print("--> Calc Score: " + name)
 
         for x in range(len(stat_order)):
             opponent = stat_order[x]
             if opponent[1] != name:
                 for t in opponent[2]:
                     if t in pokemon[13]: # Vulnerable To
-                        def_score -= 1 / (x + 1)
+                        def_score -= 10 / (x + 1)
                     if t in pokemon[12]: # Resistant To
-                        def_score += 1 / (x + 1)
+                        def_score += 10 / (x + 1)
                     if t in pokemon[11]: # Weak Against
-                        att_score -= 1 / (x + 1)
+                        att_score -= 10 / (x + 1)
                     if t in pokemon[10]: # Strong Against
-                        att_score += 1 / (x + 1)
+                        att_score += 10 / (x + 1)
 
-        _score = int(total) + att_score*20 + def_score*20
+        _score = int(int(total) + att_score + def_score)
 
         data_item = []
         data_item.append(name)
@@ -43,7 +56,7 @@ def main():
 
     data = Sort(data,1)
     print("\n\n\n")
-    show_data(data[:20])
+    show_data(data)
 
 
 
@@ -62,9 +75,10 @@ def Sort(sub_li, n):
     return sub_li 
 
 def show_data(data):
-    print("--> Show top 20")
-    for line in data:
-        
-        print(line[0] + " - " + str(line[1]))
+    print("--> Show top 20\n")
+    for x in range(len(data)):
+        line = data[x]
+        if line[1] > 0:
+            print(str(x+1) + "#  " + line[0] + " - " + str(line[1]))
 
 main()
